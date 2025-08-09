12f6c72cddfc9593458353788f0a0f514af29798
khalil <mejrik1888@gmail.com>
2025-08-09 14:54:08 +0100

    Initial commit

--- server/index.js
+++ server/index.js
@@ -274,7 +274,7 @@ updateExpiredAppointments();
 setInterval(updateExpiredAppointments, 60000);
 
 // Start server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
